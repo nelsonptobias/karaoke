f645fa349777b0ca1c17afc6cc2c4e3e834546ee
nelson <nelsonptobias@gmail.com>
2023-11-04 17:46:10 -0300

    fix cors

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 	"operations/api"
 	"os"
 
+	"github.com/gin-contrib/cors"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	// Configure o middleware CORS
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"GET", "POST", "DELETE"}
+	router.Use(cors.New(config))
 
 	router.Use(gin.Logger())
 	// Configuração das rotas da API
