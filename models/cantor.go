e2adfacb5022782154668e4a59713d49bc646218
nelson <nelsonptobias@gmail.com>
2023-11-04 18:04:20 -0300

    fix json field

--- models/cantor.go
+++ models/cantor.go
@@ -3,5 +3,5 @@ package models
 type Cantor struct {
 	Nome         string `json:"nome"`
 	Mesa         string `json:"mesa"`
-	CodigoMusica string `json:"codigo-musica"`
+	CodigoMusica string `json:"codigoMusica"`
 }
