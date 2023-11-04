0180f8a88a4f6b0862c86c80c38b9437ffc45cd6
nelson <nelsonptobias@gmail.com>
2023-11-04 12:31:51 -0300

    trying to get this start

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"operations/api"
 	"os"
 
@@ -10,11 +11,13 @@ import (
 func main() {
 	router := gin.Default()
 
+	router.Use(gin.Logger())
 	// Configuração das rotas da API
 	api.SetupRoutes(router)
 
 	port := os.Getenv("PORT")
 	if port == "" {
+		log.Fatal("$PORT must be set")
 		port = "8080" // Use a porta padrão se a variável de ambiente não estiver definida
 	}
 
